b81a5c60f5354bfa9adc207bd21d860413b3fb8e
Laboratório de Redes <labredes@Labredes.pucrs.br>
2019-08-20 09:48:15 -0300

    Tutorial 1.3 done

---
+++ iperf/iperf-2.0.10/include/iperf-int.h
@@ -0,0 +1,9 @@
+#ifndef _IPERF_INCLUDE_IPERF_INT_H
+#define _IPERF_INCLUDE_IPERF_INT_H 1
+#ifndef _GENERATED_STDINT_H
+#define _GENERATED_STDINT_H "iperf 2.0.10"
+/* generated using gnu compiler gcc (Debian 6.3.0-18+deb9u1) 6.3.0 20170516 */
+#define _STDINT_HAVE_STDINT_H 1
+#include <stdint.h>
+#endif
+#endif
